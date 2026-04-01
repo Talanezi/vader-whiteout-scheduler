2c447d292c889485f7dc0050ba8cf3725cca39f5
Talanezi <talanezi@ucsd.edu>
2026-04-01 15:39:07 -0700

    Update lower-case-naming-strategy.ts

--- server/src/lower-case-naming-strategy.ts
+++ server/src/lower-case-naming-strategy.ts
@@ -1,85 +1,28 @@
-import { DefaultNamingStrategy, NamingStrategyInterface, Table } from 'typeorm';
+import { DefaultNamingStrategy } from 'typeorm';
 
-// Adapted from https://github.com/tonivj5/typeorm-naming-strategies/blob/master/src/snake-naming.strategy.ts
-
-export default class LowerCaseNamingStrategy
-  extends DefaultNamingStrategy
-  implements NamingStrategyInterface
-{
-  tableName(targetName: string, userSpecifiedName: string | undefined) {
+export default class LowerCaseNamingStrategy extends DefaultNamingStrategy {
+  tableName(
+    targetName: string,
+    userSpecifiedName: string | undefined,
+  ): string {
     return super.tableName(targetName, userSpecifiedName).toLowerCase();
-  }
-
-  closureJunctionTableName(originalClosureTableName: string) {
-    return super
-      .closureJunctionTableName(originalClosureTableName)
-      .toLowerCase();
   }
 
   columnName(
     propertyName: string,
-    customName: string | undefined,
+    customName: string,
     embeddedPrefixes: string[],
-  ) {
+  ): string {
     return super
       .columnName(propertyName, customName, embeddedPrefixes)
       .toLowerCase();
   }
 
-  relationName(propertyName: string) {
+  relationName(propertyName: string): string {
     return super.relationName(propertyName).toLowerCase();
   }
 
-  primaryKeyName(tableOrName: Table | string, columnNames: string[]) {
-    return super.primaryKeyName(tableOrName, columnNames).toLowerCase();
-  }
-
-  uniqueConstraintName(tableOrName: Table | string, columnNames: string[]) {
-    return super.uniqueConstraintName(tableOrName, columnNames).toLowerCase();
-  }
-
-  relationConstraintName(
-    tableOrName: Table | string,
-    columnNames: string[],
-    where?: string,
-  ) {
-    return super.relationConstraintName(tableOrName, columnNames).toLowerCase();
-  }
-
-  defaultConstraintName(tableOrName: Table | string, columnName: string) {
-    return super.defaultConstraintName(tableOrName, columnName).toLowerCase();
-  }
-
-  foreignKeyName(
-    tableOrName: Table | string,
-    columnNames: string[],
-    referencedTablePath?: string,
-    referencedColumnNames?: string[],
-  ) {
-    return super
-      .foreignKeyName(tableOrName, columnNames, referencedTablePath)
-      .toLowerCase();
-  }
-
-  indexName(tableOrName: Table | string, columns: string[], where?: string) {
-    return super.indexName(tableOrName, columns, where).toLowerCase();
-  }
-
-  checkConstraintName(
-    tableOrName: Table | string,
-    expression: string,
-    isEnum?: boolean,
-  ) {
-    return super
-      .checkConstraintName(tableOrName, expression, isEnum)
-      .toLowerCase();
-  }
-
-  exclusionConstraintName(tableOrName: Table | string, expression: string) {
-    return super.exclusionConstraintName(tableOrName, expression).toLowerCase();
-  }
-
-  joinColumnName(relationName: string, referencedColumnName: string) {
+  joinColumnName(relationName: string, referencedColumnName: string): string {
     return super
       .joinColumnName(relationName, referencedColumnName)
       .toLowerCase();
@@ -90,7 +33,7 @@ export default class LowerCaseNamingStrategy
     secondTableName: string,
     firstPropertyName: string,
     secondPropertyName: string,
-  ) {
+  ): string {
     return super
       .joinTableName(
         firstTableName,
@@ -101,37 +44,29 @@ export default class LowerCaseNamingStrategy
       .toLowerCase();
   }
 
-  joinTableColumnDuplicationPrefix(columnName: string, index: number) {
-    return super
-      .joinTableColumnDuplicationPrefix(columnName, index)
-      .toLowerCase();
-  }
-
   joinTableColumnName(
     tableName: string,
     propertyName: string,
     columnName?: string,
-  ) {
+  ): string {
     return super
       .joinTableColumnName(tableName, propertyName, columnName)
       .toLowerCase();
   }
 
-  joinTableInverseColumnName(
-    tableName: string,
-    propertyName: string,
-    columnName?: string,
-  ) {
+  classTableInheritanceParentColumnName(
+    parentTableName: string,
+    parentTableIdPropertyName: string,
+  ): string {
     return super
-      .joinTableInverseColumnName(tableName, propertyName, columnName)
+      .classTableInheritanceParentColumnName(
+        parentTableName,
+        parentTableIdPropertyName,
+      )
       .toLowerCase();
   }
 
-  prefixTableName(prefix: string, tableName: string) {
-    return super.prefixTableName(prefix, tableName).toLowerCase();
-  }
-
-  eagerJoinRelationAlias(alias: string, propertyPath: string) {
-    return super.eagerJoinRelationAlias(alias, propertyPath).toLowerCase();
+  eagerJoinRelationAlias(alias: string, propertyPath: string): string {
+    return `${alias}_${propertyPath.replace(/\./g, '_')}`.toLowerCase();
   }
 }
