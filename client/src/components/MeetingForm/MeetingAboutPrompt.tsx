e95816504e37fcf8bc2ba0b3dd9f86671e9c0e0f
Talanezi <talanezi@ucsd.edu>
2026-04-01 16:20:10 -0700

    Update MeetingAboutPrompt.tsx

--- client/src/components/MeetingForm/MeetingAboutPrompt.tsx
+++ client/src/components/MeetingForm/MeetingAboutPrompt.tsx
@@ -6,26 +6,26 @@ export default function MeetingAboutPrompt({
   setMeetingAbout,
 }: {
   meetingAbout: string,
-  setMeetingAbout: (about: string) => void },
-) {
+  setMeetingAbout: (about: string) => void,
+}) {
   const onMeetingAboutChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMeetingAbout(e.target.value);
   };
+
   return (
     <Form.Group controlId="meeting-about-prompt" className="create-meeting-form-group">
       <Form.Label className="create-meeting-question">
-        What's your meeting about?
+        What is this block for?
       </Form.Label>
       <Form.Control
         as="textarea"
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
         rows={3}
-        placeholder="Super important meeting to increase productivity"
+        placeholder="Scene work, location constraints, costume needs, priority cast/crew, special equipment, sunset timing, parking, sound concerns..."
         className="form-text-input"
         value={meetingAbout}
         onChange={onMeetingAboutChange}
-      >
-      </Form.Control>
+      />
     </Form.Group>
   );
 }
