f1adf55959052ee8d2863b4939463cd404261ce8
Talanezi <talanezi@ucsd.edu>
2026-04-01 20:29:43 -0700

    Update index.ts

--- client/src/components/MeetingForm/index.ts
+++ client/src/components/MeetingForm/index.ts
@@ -1,2 +1,5 @@
-export * from './MeetingForm';
 export { default } from './MeetingForm';
+export { default as MeetingNamePrompt } from './MeetingNamePrompt';
+export { default as MeetingLocationPrompt } from './MeetingLocationPrompt';
+export { default as MeetingAboutPrompt } from './MeetingAboutPrompt';
+export { default as MeetingTimesPrompt } from './MeetingTimesPrompt';
