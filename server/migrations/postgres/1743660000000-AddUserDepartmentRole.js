17c01c274e6b6c873c4d4fd277e4f29494e7c7a9
Talanezi <talanezi@ucsd.edu>
2026-04-03 21:18:11 +0000

    Make Postgres department/role migration idempotent

--- server/migrations/postgres/1743660000000-AddUserDepartmentRole.js
+++ server/migrations/postgres/1743660000000-AddUserDepartmentRole.js
@@ -1,15 +1,15 @@
 const { MigrationInterface, QueryRunner } = require("typeorm");
 
 module.exports = class AddUserDepartmentRole1743660000000 {
-  name = 'AddUserDepartmentRole1743660000000';
+  name = "AddUserDepartmentRole1743660000000";
 
   async up(queryRunner) {
-    await queryRunner.query(`ALTER TABLE "User" ADD COLUMN "Department" character varying`);
-    await queryRunner.query(`ALTER TABLE "User" ADD COLUMN "Role" character varying`);
+    await queryRunner.query(`ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "Department" character varying`);
+    await queryRunner.query(`ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "Role" character varying`);
   }
 
   async down(queryRunner) {
-    await queryRunner.query(`ALTER TABLE "User" DROP COLUMN "Role"`);
-    await queryRunner.query(`ALTER TABLE "User" DROP COLUMN "Department"`);
+    await queryRunner.query(`ALTER TABLE "User" DROP COLUMN IF EXISTS "Role"`);
+    await queryRunner.query(`ALTER TABLE "User" DROP COLUMN IF EXISTS "Department"`);
   }
 };
